d9f450328bf2d3cc8488a20fc11c5e89181768aa
Sleepy Cat <gilb2966@gmail.com>
2023-04-10 18:43:38 -0500

    built jQuery based loop to iterate through hour array

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,71 @@ let past = $(".past"); //caching elements using jquery to access later//
 let present = $(".present");
 let future = $(".future");
 
-let today = dayjs()
-console.log(today)
-$('#currentDay').text(today.format("dddd, MMMM DD, YYYY"));
+let today = dayjs();
 
-let currentHour = today.format("H");
-console.log(currentHour); //24-hour # value//
+$("#currentDay").text(today.format("dddd, MMMM DD, YYYY"));
 
-//time is past, color gray. if time is present, set style to yellow. if time is future, set style to green)
-// let hour = [$()]
-let rows = $(".row")
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes?
-rows.each( function(index, element ) { //working; iterating over jQuery collection stored in "rows" variable//
-  // if ();
-});
-//if/else to compare; for loop to select or list
+let currentHour = today.format("H"); //24-hour # value//
+// let currentHour = 14;
 
+let rows = $(".row");
+
+let hour = [
+  {
+    id: "#hour-8",
+    hour: 8,
+  },
+  {
+    id: "#hour-9",
+    hour: 9,
+  },
+  {
+    id: "#hour-10",
+    hour: 10,
+  },
+  { id: "#hour-11", hour: 11 },
+  {
+    id: "#hour-12",
+    hour: 12,
+  },
+  {
+    id: "#hour-13",
+    hour: 13,
+  },
+  {
+    id: "#hour-14",
+    hour: 14,
+  },
+  {
+    id: "#hour-15",
+    hour: 15,
+  },
+  {
+    id: "#hour-16",
+    hour: 16,
+  },
+  {
+    id: "#hour-17",
+    hour: 17,
+  },
+];
+
+$.each(hour, function (key, value) {
+    let hourInt = value.hour
+    let rowId = value.id
+    if (hourInt < currentHour) { //working!
+      console.log(rowId);
+      $(rowId).attr("class", "row time-block past")
+    }
+    else if (hourInt === currentHour) {
+      console.log(rowId);
+      $(rowId).attr("class", "row time-block present")
+    }
+    else if (hourInt > currentHour) {
+      console.log(rowId);
+      $(rowId).attr("class", "row time-block future")
+    }
+})
 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
@@ -38,11 +84,6 @@ $(".saveBtn").on("click", function (e) {
   e.preventDefault();
 });
 
-// Use class for "past", "present", and "future" to apply styles to the
-//         time-block divs accordingly. The javascript will need to do this by
-//         adding/removing these classes on each div by comparing the hour in the
-//         id to the current hour. The html provided below is meant to be an example
-//         demonstrating how the css provided can be leveraged to create the
-//         desired layout and colors. The html below should be removed or updated
-//         in the finished product. Remember to delete this comment once the
-//         code is implemented.
+
+
+
