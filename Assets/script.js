8add69bd7fc06277f684f9bca7216936be067ea0
Sleepy Cat <gilb2966@gmail.com>
2023-04-10 21:35:47 -0500

    moved files removed Develop dir and built README

--- Assets/script.js
+++ Assets/script.js
@@ -7,8 +7,8 @@ let today = dayjs();
 
 $("#currentDay").text(today.format("dddd, MMMM DD, YYYY"));
 
-let currentHour = today.format("H"); //24-hour # value//
-// let currentHour = 14;
+// let currentHour = today.format("H"); //24-hour # value//
+let currentHour = 11;
 
 let rows = $(".row");
 
@@ -69,21 +69,25 @@ $.each(hour, function (key, value) {
     }
 })
 
-// TODO: Add a listener for click events on the save button. This code should
+// TODO: This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
 // function? How can DOM traversal be used to get the "hour-x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
 
+//jQuery to get all div ids in one array
+//click bubbles to parent element div w/ id
 $(".saveBtn").on("click", function (e) {
   e.preventDefault();
+  console.log(e.target.id);
+  //$.map maybe?
 });
 
 
 
 
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
---
+++ README.md
@@ -0,0 +1,27 @@
+# Work Day Scheduler JavaScript & jQuery
+
+Welcome to my JavaScript practice project using jQuery. Here, an HTML and CSS template are provided and we are tasked with building the JavaScript using various jQuery (https://learn.jquery.com/using-jquery-core/) methods and event handlers. jQuery is a third-party API that provides concise methods for traversing & manipulating the DOM; additionally, jQuery can provide "syntactic sugar" to JavaScript, reducing the at times tedious nature of accessing HTML elements in the DOM via traditional "vanilla" JavaScript.
+
+## Usage
+
+This project is a simple event scheduler that provides color-coded rows which dynamically change color based on the time of day. This function provides visual cues to the user to alert them of any past, present or upcoming tasks that may have been recorded in the corresponding row(s). There is also a save feature that allows the user to save their tasks to local storage, which means that those tasks will persist in memory even upon page refresh.
+
+## Links
+
+Please see the links below for both GitHub and the deployed page available via GitHub Pages:
+
+GitHub: https://github.com/buster35/synevyr-lacus
+
+GitHub Pages:
+
+## Screenshots
+
+![](./Assets/work-day-scheduler.png)
+
+## License
+
+Please refer to the LICENSE in the repository.
+
+
+
+
